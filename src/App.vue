1defe7e7b74bf27a38401d901f5e5e45a4d2e46d
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-11-28 15:35:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+  <div class="container text-center">
+    <AppTitle/>
+  </div>
+</template>
+
+<style lang="scss">
+@import './style/global.scss';
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      title: 'Il titolo della pagina'
+    }
+  }
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  @include media-breakpoint-up(md) {
+    color: red;
+  }
+}
+</style>
